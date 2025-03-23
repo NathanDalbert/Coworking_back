f0d665e93912a3d0b56ccfb54fee35838f78b111
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-23 18:21:49 -0300

    chore(estrutura): reorganizar estrutura de pastas do projeto

---
+++ src/main/java/com/Coworking/demo/modules/usuario/repository/Usuario.java
@@ -0,0 +1,12 @@
+package com.Coworking.demo.modules.usuario.repository;
+
+import java.util.UUID;
+
+public class Usuario {
+    UUID id;
+    String nome;
+    String email;
+    String senha;
+    String telefone;
+
+}
