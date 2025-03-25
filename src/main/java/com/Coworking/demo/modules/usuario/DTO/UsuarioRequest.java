cd404323e3d11a6e3b6ae346b5f49405a55c6f1d
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-25 18:42:21 -0300

    refactor: refatorar UsuarioRequest

--- src/main/java/com/Coworking/demo/modules/usuario/DTO/UsuarioRequest.java
+++ src/main/java/com/Coworking/demo/modules/usuario/DTO/UsuarioRequest.java
@@ -1,4 +1,4 @@
 package com.Coworking.demo.modules.usuario.DTO;
 
-public record UsuarioRequest(String nome, String email, String senha, String fotoPerfil, Boolean integridade) {
+public record UsuarioRequest(String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
 }
