fb0618f57da47cb5c7e690532ae4f8e8fea05f6a
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-25 18:44:26 -0300

    refactor: refatorar classe usuario

--- src/main/java/com/Coworking/demo/modules/usuario/domain/Usuario.java
+++ src/main/java/com/Coworking/demo/modules/usuario/domain/Usuario.java
@@ -19,6 +19,10 @@ public class Usuario {
     @Column(name = "nome")
    private String nome;
 
+    @Column(name = "dataNascimento")
+    private String dataNascimento;
+
+
     @Column(name = "email")
    private String email;
     @Column(name = "senha")
@@ -28,17 +32,18 @@ public class Usuario {
     @Column(name = "integridade")
     private Boolean integridade;
 
-    private Usuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade) {
+    private Usuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
 
         this.nome = nome;
         this.email = email;
         this.senha = senha;
         this.fotoPerfil = fotoPerfil;
         this.integridade = integridade;
+        this.dataNascimento = dataNascimento;
     }
 
-    public static  Usuario newUsuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade){
-        return new Usuario (nome, email, senha, fotoPerfil, integridade);
+    public static  Usuario newUsuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
+        return new Usuario (nome, email, senha, fotoPerfil, integridade, dataNascimento );
     }
 
 
