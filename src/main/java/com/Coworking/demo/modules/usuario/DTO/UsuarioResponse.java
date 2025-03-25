36e1d696bd1ce48d05ac8f25d08f5ac58b30af2c
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-25 18:43:29 -0300

    refactor: refatorar UsuariorESPONSE

--- src/main/java/com/Coworking/demo/modules/usuario/DTO/UsuarioResponse.java
+++ src/main/java/com/Coworking/demo/modules/usuario/DTO/UsuarioResponse.java
@@ -2,5 +2,5 @@ package com.Coworking.demo.modules.usuario.DTO;
 
 import java.util.UUID;
 
-public record UsuarioResponse(UUID id, String nome, String email, String fotoPerfil, Boolean integridade) {
+public record UsuarioResponse(UUID id, String nome, String email, String fotoPerfil, Boolean integridade, String dataNascimento) {
 }
