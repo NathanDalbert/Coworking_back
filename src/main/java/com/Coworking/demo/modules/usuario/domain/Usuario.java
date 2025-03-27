3a2a99cdfd3ec0a22688ded22f188b14f7ed17db
NathanDalbert <dalbert.nathan@gmail.com>
2025-03-26 22:50:25 -0300

    fix: corrigir logica de gerar id automatico

--- src/main/java/com/Coworking/demo/modules/usuario/domain/Usuario.java
+++ src/main/java/com/Coworking/demo/modules/usuario/domain/Usuario.java
@@ -4,36 +4,39 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
-@Entity( name = "usuario")
-@Table( name = "usuario")
+@Entity(name = "usuario")
+@Table(name = "usuario")
 public class Usuario {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private UUID id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
-    @Column(name = "nome")
-   private String nome;
+    @Column(name = "nome", nullable = false)
+    private String nome;
 
-    @Column(name = "dataNascimento")
+    @Column(name = "data_nascimento", nullable = false)
     private String dataNascimento;
 
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
-    @Column(name = "email")
-   private String email;
-    @Column(name = "senha")
-   private String senha;
-    @Column(name = "fotoPerfil")
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "foto_perfil")
     private String fotoPerfil;
-    @Column(name = "integridade")
+
+    @Column(name = "integridade", nullable = false)
     private Boolean integridade;
 
-    private Usuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
 
+    public Usuario(String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
@@ -42,9 +45,8 @@ public class Usuario {
         this.dataNascimento = dataNascimento;
     }
 
-    public static  Usuario newUsuario( String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
-        return new Usuario (nome, email, senha, fotoPerfil, integridade, dataNascimento );
+
+    public static Usuario newUsuario(String nome, String email, String senha, String fotoPerfil, Boolean integridade, String dataNascimento) {
+        return new Usuario(nome, email, senha, fotoPerfil, integridade, dataNascimento);
     }
-
-
 }
